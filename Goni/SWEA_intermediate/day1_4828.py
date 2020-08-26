0fad859ae63fc54b7488723a6177673fe1a7337c
gonipark <gonini1@naver.com>
2020-08-27 03:13:29 +0900

    add directory

---
+++ Goni/SWEA_intermediate/day1_4828.py
@@ -0,0 +1,12 @@
+import sys
+sys.stdin=open("sample_input.txt",'r')
+
+T=int(input())
+
+for test_case in range(1,T+1):
+    N=int(input())
+    lst=list(map(int,input().split()))
+
+    print("#", test_case, sep='', end=' ')
+    print(max(lst)-min(lst))
+
