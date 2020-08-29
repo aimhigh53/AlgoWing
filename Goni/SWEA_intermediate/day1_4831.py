4adce866eb53198ae99c1d0403bfac40eb79d4e9
gonipark <gonini1@naver.com>
2020-08-29 23:05:44 +0900

    전기버스

--- Goni/SWEA_intermediate/day1_4831.py
+++ Goni/SWEA_intermediate/day1_4831.py
@@ -1,34 +1,32 @@
 import sys
 sys.stdin=open("sample_input.txt",'r')
 
-T = int(input())
+T=int(input())
 
-for test_case in range(1, 1 + T):
-    K, N, M = map(int, input().split())
-    temp = list(map(int, input().split()))
-    station = [0] * N
-    for i in temp:
-        station[i] = 1
-    now = K
-    count = 1
-    while (True):
-        now += K
-        if now >= N:
-            print("#", test_case, sep='', end=' ')
-            print(count)
+for test_case in range(1,T+1):
+    max,station_num,recharge_num=map(int,input().split())
+    recharge_station=list(map(int,input().split()))
+    now=0
+    count=0
+    breaker=False
+    while(True):
+        for i in range(max,0,-1):
+            if now+max >= station_num:
+                breaker=True
+                break
+            elif now+i in recharge_station:
+                count+=1
+                now+=i
+                break
+            if i==1:
+                count=0
+                breaker = True
+                break
+        if breaker:
             break
 
-        if station[now] == 1:
-            count += 1
-        else:
-            for j in range(1, K):
-                temp = count
-                now -= 1
-                if station[now] == 1:
-                    count += 1
-                    break
 
-            if temp == count:
-                print("#", test_case, sep='', end=' ')
-                print('0')
-                break
+
+
+    print("#", test_case, sep='', end=' ')
+    print(count)
