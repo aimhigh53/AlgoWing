c24ca285148d747bd04bfea54d56697764130da0
gonipark <gonini1@naver.com>
2020-08-29 23:20:45 +0900

    숫자카드

---
+++ Goni/SWEA_intermediate/day1_4834.py
@@ -0,0 +1,22 @@
+import sys
+sys.stdin=open("sample_input.txt",'r')
+T=int(input())
+
+for test_case in range(1,T+1):
+    N=int(input())
+    temp=input()
+    nums=[]
+    for i in temp:
+        nums.append(int(i))
+    keys=[i for i in range(0,10)]
+    count_dic=dict.fromkeys(keys,0)
+
+    for num in nums:
+        count_dic[num]+=1
+
+    max=0
+    for i in keys:
+        if max<=count_dic[i]:
+            max=count_dic[i]
+            key=i
+    print("#{} {} {}".format(test_case,key,max))
