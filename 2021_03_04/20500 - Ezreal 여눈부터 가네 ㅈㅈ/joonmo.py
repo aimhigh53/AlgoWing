c595ef4a80579f71718ef00fe2015680df99fd74
jack9410 <jack941028@naver.com>
2021-03-08 22:01:03 +0900

    3.8 joonmo

---
+++ 2021_03_04/20500 - Ezreal 여눈부터 가네 ㅈㅈ/joonmo.py
@@ -0,0 +1,14 @@
+import os
+import sys
+curr_dir = os.path.dirname(__file__)
+sys.stdin = open(curr_dir + '\input.txt', 'r')
+
+dp = [[0 for _ in range(3)] for _ in range(1516)]
+dp[1][1] = 1
+for i in range(2, 1516):
+    dp[i][0] = dp[i-1][1] + dp[i-1][2]
+    dp[i][1] = dp[i-1][0] + dp[i-1][2]
+    dp[i][2] = dp[i-1][0] + dp[i-1][1]
+    for j in range(3):
+        dp[i][j] %= 1000000007
+print(dp[int(input())][0])
